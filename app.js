ce97883056cb704a6cb8ffbf4c70853f6575f0e1
RebeccaJ21 <rebecca.prows@gmail.com>
2017-11-10 01:34:32 -0800

    7-questions'

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 'use strict';
 
+var answersright=0;
+
 var allergy = prompt('Am I allergic to chocolate?').toLowerCase();
 console.log('fake-allergy', allergy);
 
 if (allergy === 'no' || allergy === 'n') {
   alert('You know me!');
+  answersright+=1;
 }
 else if (allergy === 'yes' || allergy === 'y') {
   alert('WRONG');
@@ -18,12 +21,14 @@ console.log('real-siblings', siblings);
 
 if ((allergy === 'no' || allergy === 'n') && (siblings === 'yes' || siblings === 'y')) {
   alert('You know me very well!');
+  answersright+=1;
 }
 else if ((allergy === 'no' || allergy === 'n') && (siblings === 'no' || siblings === 'n')) {
   alert('Well, at least you got the first one right!');
 }
 else if ((allergy === 'yes' || allergy === 'y') && (siblings === 'yes' || siblings === 'y')) {
   alert('You got this one right!');
+  answersright+=1;
 }
 else if ((allergy === 'yes' || allergy === 'y') && (siblings === 'no' || siblings === 'n')) {
   alert('Wrong Again!');
@@ -34,10 +39,11 @@ else {
 
 
 var question = prompt('Did I spend too much time trying to think of a yes or no question?').toLowerCase();
-console.log('over-thinking question', question);
+console.log('over-thinking-question', question);
 
 if (question === 'yes' || question === 'y') {
   alert('You know it!');
+  answersright+=1;
 }
 else {
   alert('Trust me, I really did.');
@@ -48,6 +54,7 @@ console.log('not-yellow', color);
 
 if (color === 'no' || color === 'n') {
   alert('Correct! My favorite color is blue!');
+  answersright+=1;
 }
 else if (color == 'yes' || color == 'y') {
   alert('Nope! Blue is my favorite color!');
@@ -57,17 +64,54 @@ else {
 }
 
 var naps = prompt('Am I a fan of naps?').toLowerCase();
-console.log('napsarebest', naps);
+console.log('naps-are-best', naps);
 
 if (naps === 'yes' || naps === 'y') {
   alert('Of course! I love naps!');
+  answersright+=1;
 }
 else if (naps === 'no' || naps === 'n') {
   alert('What? Naps are the best.');
 }
 var job=prompt('How many years have I worked at my current job?').toLowerCase();
+var counter=0;
+while (counter<4 && job!=7) {
+console.log('Guess-again!');
+  if (job<7) {
+  job=prompt('That was too low, guess again! How many years have I worked at my current job?');
+} else if (job>7) {
+  job=prompt('That was too high, guess again! How many years have I worked at my current job?');
+} counter+=1;
+}
+    if (job<7) {
+      alert('That was too low. The correct answer is 7.');
+  } else if (job>7) {
+      alert('That was too high. The correct answer is 7.');
+  }
+    else{
+      alert('7 is correct!')
+      answersright+=1;
+    }
 console.log('jobtoolong',job);
 
+var travel=prompt('Name one of my Top Three Countries I want to travel too.');
+var counter=0;
+while (counter<6 && travel!='Ireland' && travel!='Scotland' && travel!='Italy') {
+  console.log('top-three');
+  console.log(travel);
+  travel= prompt('Not in my current top three! Guess Again!');
+  counter+=1;
+ }
+  console.log('top-three-travel');
+  if (travel!='Ireland' && travel!='Scotland' && travel!='Italy') {
+  alert('Good Try! My current top three countries are Ireland, Scotland, and Italy');
+} else {
+    alert('Correct! My top three countries are Ireland, Scotland, and Italy!');
+    answersright+=1;
+}
+  console.log('3-travel');
+
+  alert('You got '+ answersright +' Out of 7');
 
 var el=document.getElementById('first');
 el.textContent='Am I allergic to chocolate? Correct Answer: No';
